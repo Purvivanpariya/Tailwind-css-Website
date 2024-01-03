0f7b3b24e35ab15a78da283efc6765e8d256520c
Purvivanpariya <purvipatel4503@gmail.com>
2024-01-03 17:42:35 +0530

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+  theme: {
+    fontFamily: {
+      'sans': ['Poppins', 'sans-serif'],
+      'serif': ['Roboto', 'sans-serif'],
+    },
+  container :{
+      screens: {
+      sm: '600px',
+      md: '728px',
+      lg: '984px',
+      xl: '1020px',
+      '2xl': '1020px',
+    }
+  }
+  }
+}
